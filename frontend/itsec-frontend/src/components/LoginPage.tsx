f2acdd826600073ad4a03bdf3430376e45d36e0c
Bálint Nagy <info.balint.nagy@edu.bme.hu>
2022-11-26 18:35:16 +0100

    Add login and register page

--- frontend/itsec-frontend/src/components/LoginPage.tsx
+++ frontend/itsec-frontend/src/components/LoginPage.tsx
@@ -1,8 +1,169 @@
+import { Alert, AppBar, Avatar, Box, Button, Grid, Link, TextField, Toolbar, Typography } from "@mui/material";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+    loginError: false,
+    loginMessage: '',
+    errorMsgEmail: '',
+    errorMsgPassword: '',
+  });
+
+  const validateEmail = (value: any) => {
+    if (!value) return 'Please enter your email address.';
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(value)) return 'Invalid email address.';
+    return '';
+  }
+
+  const validatePassword = (password: any) => {
+    if (!password) return 'Please enter your password.';
+    return '';
+  }
+
+  const sendForm = (payload: any): any => {
+    //TODO
+    console.log(payload);
+    return {ok: true};
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    let email = data.get('email');
+    let password = data.get('password');
+
+    let emailErr = validateEmail(email);
+    let passwordErr = validatePassword(password);
+
+    setFormState({
+      ...formState,
+      errorMsgEmail: emailErr,
+      errorMsgPassword: passwordErr,
+    });
+    
+    if (!emailErr && !passwordErr) {
+      let response = await sendForm(formState);
+      if (response.ok) {
+        // User registered successfully
+        setFormState({
+          ...formState,
+          errorMsgEmail: emailErr,
+          errorMsgPassword: passwordErr,
+          loginMessage: 'Logged in successfully!',
+          loginError: false
+        });
+        await new Promise(res => setTimeout(res, 2000));
+        navigate('/');
+      }
+      else {
+        // Server encountered error
+        setFormState({
+          ...formState,
+          errorMsgEmail: emailErr,
+          errorMsgPassword: passwordErr,
+          loginMessage: 'Failed to login. Please try again later.',
+          loginError: true
+        });
+      }
+    }
+  };
+
+  const handlePasswordRecovery = () => {
+    // Todo?
+  }
+
+  const handleRegister = () => {
+    navigate('/register');
+  }
+
     return (
-      <div>
-        Login
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography align="left" variant="h6" component="div" sx={{ flexGrow: 1 }} color="black">
+              ITSecTa
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          sx={{
+            marginTop: '160px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  helperText={formState.errorMsgEmail}
+                  error={formState.errorMsgEmail !== ''}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  helperText={formState.errorMsgPassword}
+                  error={formState.errorMsgPassword !== ''}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            {formState.loginMessage && (
+              <Alert severity={formState.loginError ? "error" : "info"}>{formState.loginMessage}</Alert>
+            )}
+            <Grid container justifyContent="flex-end">
+              <Grid item xs>
+                <Link href='' onClick={handlePasswordRecovery} variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item xs>
+                <Link href='' onClick={handleRegister} variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Box>
     );
   }
   
--- frontend/itsec-frontend/src/components/RegisterPage.tsx
+++ frontend/itsec-frontend/src/components/RegisterPage.tsx
@@ -1,8 +1,188 @@
+import { Alert, AppBar, Avatar, Box, Button, Grid, Link, TextField, Toolbar, Typography } from "@mui/material";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
+
 const RegisterPage = () => {
-    return (
-      <div>
-        Register
-      </div>
+  const navigate = useNavigate();
+
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    registrationError: false,
+    registrationMessage: '',
+    errorMsgEmail: '',
+    errorMsgPassword: '',
+    errorMsgConfirmPassword: ''
+  });
+  
+  const validateEmail = (value: any) => {
+    if (!value) return 'Please enter your email address.';
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(value)) return 'Invalid email address.';
+    return '';
+  }
+
+  const validatePassword = (password: any) => {
+    if (!password) return 'Please enter your password.';
+    if ((password as string).length < 7) return 'Password must be at least 7 characters long.';
+    return '';
+  }
+
+  const validatePasswordConfirm = (password: any, passwordConfirmation: any) => {
+    if (!passwordConfirmation) return 'Please re-enter your password.';
+    if (password !== passwordConfirmation) return 'Incorrect password confirmation.';
+   return '';
+  }
+
+  const sendForm = (payload: any): any => {
+    //TODO
+    console.log(payload);
+    return {ok: true};
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget); 
+
+    let email = data.get('email');
+    let password = data.get('password');
+    let passwordConfirm = data.get('passwordConfirmation');
+
+    let emailErr = validateEmail(email);
+    let passwordErr = validatePassword(password);
+    let passwordConfirmErr = validatePasswordConfirm(password, passwordConfirm);
+
+    setFormState({
+      ...formState,
+      registrationMessage: '',
+      registrationError: false,
+      errorMsgEmail: emailErr,
+      errorMsgPassword: passwordErr,
+      errorMsgConfirmPassword: passwordConfirmErr
+    });
+
+    if (!emailErr && !passwordErr && !passwordConfirmErr) {
+      let response = await sendForm(formState);
+      if (response.ok) {
+        // User registered successfully
+        setFormState({
+          ...formState,
+          errorMsgEmail: emailErr,
+          errorMsgPassword: passwordErr,
+          errorMsgConfirmPassword: passwordConfirmErr,
+          registrationMessage: 'Registration successful!',
+          registrationError: false
+        });
+        await new Promise(res => setTimeout(res, 2000));
+        navigate('/login');
+      }
+      else {
+        // Server encountered error
+        setFormState({
+          ...formState,
+          errorMsgEmail: emailErr,
+          errorMsgPassword: passwordErr,
+          errorMsgConfirmPassword: passwordConfirmErr,
+          registrationMessage: 'Failed to register. Please try again later.',
+          registrationError: true
+        });
+      }
+    }
+  };
+
+  const handleLogin = () => {
+    navigate('/login');
+  }
+
+  return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography align="left" variant="h6" component="div" sx={{ flexGrow: 1 }} color="black">
+              ITSecTa
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          sx={{
+            marginTop: '160px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  helperText={formState.errorMsgEmail}
+                  error={formState.errorMsgEmail !== ''}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  helperText={formState.errorMsgPassword}
+                  error={formState.errorMsgPassword !== ''}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="passwordConfirmation"
+                  label="Confirm password"
+                  type="password"
+                  id="passwordConfirmation"
+                  autoComplete="new-password"
+                  helperText={formState.errorMsgConfirmPassword}
+                  error={formState.errorMsgConfirmPassword !== ''}
+                />
+              </Grid>
+            </Grid>
+            {formState.registrationMessage && (
+              <Alert severity={formState.registrationError ? "error" : "info"}>{formState.registrationMessage}</Alert>
+            )}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="" onClick={handleLogin} variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Box>
     );
   }
   
