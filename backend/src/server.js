ad892920abfbb938b461d1e826ed7273c159588f
bojtosvivi <bojtosvivi@gmail.com>
2022-12-04 20:17:39 +0100

    refactor: format the code remove console log

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,49 +1,51 @@
-import express from "express";
-import fileupload from "express-fileupload";
-import cors from "cors";
+import express from 'express';
+import fileupload from 'express-fileupload';
+import cors from 'cors';
 import { passwordStrength } from 'check-password-strength';
-import { addUser,
-   setUserData , 
-   isEmailValid, 
-   isExistRegistration,
-   isValidUser,
-   getUsersForAdmin,
-   modifyUserByAdmin,
-   deleteUserByAdmin,
-   authenticateToken,
-   authenticateAdminToken
-  } from "./user-helper.js";
+import {
+	addUser,
+	setUserData,
+	isEmailValid,
+	isExistRegistration,
+	isValidUser,
+	getUsersForAdmin,
+	modifyUserByAdmin,
+	deleteUserByAdmin,
+	authenticateToken,
+	authenticateAdminToken,
+} from './user-helper.js';
 import { getCaffCollection,
-   uploadCaffFile,
-   deleteCaffByAdmin,
-   getCommentsByCaffId, 
-   addCommentToCaff,
-   deleteCommentByAdmin,
-   modifyCommentByAdmin,
-   getCaffByCaffId } from "./caff-helper.js";
-import dotenv from 'dotenv'; 
-
-//const upload_folder_path = __dirname + "/../temp/";
+  uploadCaffFile,
+  deleteCaffByAdmin, 
+  getCommentsByCaffId,
+  addCommentToCaff, 
+  deleteCommentByAdmin,
+  modifyCommentByAdmin,
+  getCaffByCaffId } from './caff-helper.js';
+import dotenv from 'dotenv';
 import path from 'path';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-
-// 👇️ "/home/john/Desktop/javascript"
 const __dirname = path.dirname(__filename);
-const output_directory = path.join(__dirname, "/../temp/");
+const output_directory = path.join(__dirname, '/../temp/');
 
 const app = express();
+
 app.use(
-  fileupload({
-      createParentPath: true,
-  }),
-); 
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://localhost:3000'],
-  methods: ['GET', 'PUT', 'POST', 'DELETE'],
-  allowedHeaders: ['Accept', 'Content-Type', 'Authorization']
-}));
+	fileupload({
+		createParentPath: true,
+	})
+);
+
+app.use(
+	cors({
+		origin: ['http://localhost:3000', 'https://localhost:3000'],
+		methods: ['GET', 'PUT', 'POST', 'DELETE'],
+		allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+	})
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -51,152 +53,136 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 
 //USER ENDPOINTS
-app.post('/user/login', async (req,res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+app.post('/user/login', async (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
 
-  if (!email || !password){
-    return res.status(400).send({
-      message: "Bad request. Email or password missing."
-    });
-  }
-  const result = await isValidUser(email, password);
-  res.status(result.code).send(result.data);
-})
-
-
-app.post("/user/modify", authenticateToken, async (req, res) => {
-    console.log(req.user);
-    const result = await setUserData(req.body, req.user);
-    return res.status(result.code).send(result.data);
+	if (!email || !password) {
+		return res.status(400).send({
+			message: 'Bad request. Email or password missing.',
+		});
+	}
+	const result = await isValidUser(email, password);
+	return res.status(result.code).send(result.data);
 });
 
-app.post("/user/register", async (req, res) => {
-  console.log(req.body);
-  console.log(req.body.email);
-  console.log(req.body.password);
-  const email = req.body.email;
-  const password = req.body.password;
+app.post('/user/modify', authenticateToken, async (req, res) => {
+	const result = await setUserData(req.body, req.user);
+	return res.status(result.code).send(result.data);
+});
 
-  if (!email || !password){
-    return res.status(400).send({
-      message: "Bad request.Email or password missing."
-    })
-  }
-  const {valid, reason, validators} = await isEmailValid(email);
+app.post('/user/register', async (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
 
-  if (!valid) {
-    return res.status(400).send({
-      message: "Please provide a valid email address.",
-      reason: validators[reason].reason
-    });
-  } 
+	if (!email || !password) {
+		return res.status(400).send({
+			message: 'Bad request.Email or password missing.',
+		});
+	}
+	const { valid, reason, validators } = await isEmailValid(email);
 
-  if(await isExistRegistration(email)) {
-    return res.status(400).send({
-      message: "Email address is already used.",
-    });
-  }
+	if (!valid) {
+		return res.status(400).send({
+			message: 'Please provide a valid email address.',
+			reason: validators[reason].reason,
+		});
+	}
 
-  if(await passwordStrength(password).value !== 'Strong') {
-    return res.status(400).send({
-      message: "Please provide a strong password",
-    });
-  } 
+	if (await isExistRegistration(email)) {
+		return res.status(400).send({
+			message: 'Email address is already used.',
+		});
+	}
 
-  let result_id;
-  try {
-    result_id = await addUser(req.body.email, req.body.password);
-  } catch (error) {
-    return res.status(500).send({message: "Internal server error. Could not created the registration."})
-  } 
-  return res.status(201).send({message: "OK", id: result_id});
+	if ((await passwordStrength(password).value) !== 'Strong') {
+		return res.status(400).send({
+			message: 'Please provide a strong password',
+		});
+	}
+  let result;
+	try {
+		result = await addUser(req.body.email, req.body.password);
+	} catch (error) {
+		return res.status(500).send({ message: 'Internal server error. Could not created the registration.' });
+	}
+	return res.status(result.code).send(result.data);
 });
 
 //ADMIN ENPOINTS
-app.get('/admin/users', authenticateAdminToken, async (req,res) => {
-  const result = await getUsersForAdmin();
-  return res.status(result.code).send(result.data);
+app.get('/admin/users', authenticateAdminToken, async (_, res) => {
+	const result = await getUsersForAdmin();
+	return res.status(result.code).send(result.data);
 });
 
 app.post('/admin/modify', authenticateAdminToken, async (req, res) => {
-  console.log(req.body);
-  const result = await modifyUserByAdmin(req.body, req.user);
-  return res.status(result.code).send(result.data);
+	const result = await modifyUserByAdmin(req.body, req.user);
+	return res.status(result.code).send(result.data);
 });
 
 app.post('/admin/delete', authenticateAdminToken, async (req, res) => {
-  console.log(req.body);
-  const result =  await deleteUserByAdmin(req.body, req.user);
-  return res.status(result.code).send(result.data);
+	const result = await deleteUserByAdmin(req.body, req.user);
+	return res.status(result.code).send(result.data);
 });
 
 //CAFF ENDPOINTS
-app.get('/caff/preview', async (req,res) => {
-  const result = await getCaffCollection();
-  return res.status(result.code).send(result.data);
+app.get('/caff/preview', async (_, res) => {
+	const result = await getCaffCollection();
+	return res.status(result.code).send(result.data);
 });
 
-app.post('/caff/upload', async (req, res) => {
-  try {
-    if (!req.files) {
-        res.send({
-            status: "failed",
-            message: "No file uploaded",
-        });
-    } else {
-        let file = req.files.file
-        console.log(file);
-        
-        await file.mv(output_directory + file.name);
-        console.log("upload mappa utan");
+app.post('/caff/upload', authenticateToken, async (req, res) => {
+	try {
+		if (!req.files) {
+			return res.status(400).send({ message: 'No file uploaded.' });
+		} else {
+			let file = req.files.file;
+			if (file.name.split('.')[1] != 'caff') return res.status(400).send({ message: 'Invalid format. This is not a .caff file.' });
+			await file.mv(output_directory + file.name);
 
-        const result = await uploadCaffFile(file.name);
-        return res.status(result.code).send(result.data);
-    } 
-  } catch (err) {
-      res.status(500).send({ message: err });
-  }
+			const result = await uploadCaffFile(file.name);
+			return res.status(result.code).send(result.data);
+		}
+	} catch (error) {
+		return res.status(500).send({ message: 'Internal server error. The given CAFF file is invalid.' });
+	}
 });
 
-app.get('/caff/purchase/:caffId', async (req, res) => {
- if(!req.params.caffId) res.status(404).send({message: "Not found"});
-  const result = await getCaffByCaffId(req.params.caffId);
-  return res.status(result.code).send(result.data);
-}); 
+app.get('/caff/purchase/:caffId', authenticateToken, async (req, res) => {
+	if (!req.params.caffId) res.status(404).send({ message: 'Not found' });
+	const result = await getCaffByCaffId(req.params.caffId);
+	return res.status(result.code).send(result.data);
+});
 
-app.delete('/caff/delete', authenticateAdminToken, async (req,res) => {
-  const result = await deleteCaffByAdmin(req.body);
-  return res.status(result.code).send(result.data);
+app.delete('/caff/delete/:caffId', authenticateAdminToken, async (req, res) => {
+	if (!req.params.caffId) res.status(404).send({ message: 'Not found' });
+	const result = await deleteCaffByAdmin(req.params.caffId);
+	return res.status(result.code).send(result.data);
 });
 
 //COMMENTS ENDPOINTS
-app.get('/comments/:caffId', async (req,res) => {
-  console.log(req.params.caffId);
-  if(!req.params.caffId) res.status(404).send({message: "Not found"});
-  const result = await getCommentsByCaffId(req.params.caffId);
-  return res.status(result.code).send(result.data);
+app.get('/comments/:caffId', async (req, res) => {
+	if (!req.params.caffId) res.status(404).send({ message: 'Not found' });
+	const result = await getCommentsByCaffId(req.params.caffId);
+	return res.status(result.code).send(result.data);
 });
 
 app.post('/comments/add/:caffId', authenticateToken, async (req, res) => {
-  console.log(req.params.caffId);
-  if(!req.params.caffId) res.status(404).send({message: "Not found"});
-  const result = await addCommentToCaff(req.params.caffId, req.body);
-  return res.status(result.code).send(result.data);
+	if (!req.params.caffId) res.status(404).send({ message: 'Not found' });
+	const result = await addCommentToCaff(req.params.caffId, req.body);
+	return res.status(result.code).send(result.data);
 });
 
-app.delete('/comments/delete/:caffId', authenticateAdminToken, async (req,res) => {
-  console.log(req.params.caffId);
-  if(!req.params.caffId) res.status(404).send({message: "Not found"});
-  const result = await deleteCommentByAdmin(req.params.caffId, req.body);
-  return res.status(result.code).send(result.data);
+app.delete('/comments/delete/:caffId', authenticateAdminToken, async (req, res) => {
+	if (!req.params.caffId) res.status(404).send({ message: 'Not found' });
+	const result = await deleteCommentByAdmin(req.params.caffId, req.body);
+	return res.status(result.code).send(result.data);
 });
 
-app.post('/comments/modify/:caffId', authenticateAdminToken , async (req, res) => {
-  console.log(req.params.caffId);
-  if(!req.params.caffId) res.status(404).send({message: "Not found"});
-  const result = await modifyCommentByAdmin(req.params.caffId, req.body);
-  return res.status(result.code).send(result.data);
+app.post('/comments/modify/:caffId', authenticateAdminToken, async (req, res) => {
+	if (!req.params.caffId) res.status(404).send({ message: 'Not found' });
+	const result = await modifyCommentByAdmin(req.params.caffId, req.body);
+	return res.status(result.code).send(result.data);
 });
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- backend/src/user-helper.js
+++ backend/src/user-helper.js
@@ -1,229 +1,219 @@
-import { MongoClient } from "mongodb";
-import emailValidator from "deep-email-validator";
-import * as argon2 from "argon2";
+import { MongoClient } from 'mongodb';
+import emailValidator from 'deep-email-validator';
+import * as argon2 from 'argon2';
 import { passwordStrength } from 'check-password-strength';
-import * as crypto from "crypto";
+import * as crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 
 const url = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
-const database = client.db("itsecta");
-const users = database.collection("user");
+const database = client.db('itsecta');
+const users = database.collection('user');
 
-const hashingConfig = { // based on OWASP cheat sheet recommendations (as of March, 2022)
-    parallelism: 1,
-    memoryCost: 64000, // 64 mb
-    timeCost: 3 // number of itetations
-}
+// based on OWASP cheat sheet recommendations (as of March, 2022)
+const hashingConfig = {
+	parallelism: 1,
+	memoryCost: 64000, // 64 mb
+	timeCost: 3, // number of itetations
+};
 
 async function hashPassword(password) {
-    let salt = crypto.randomBytes(16);
-    return await argon2.hash(password, {
-        ...hashingConfig,
-        salt,
-    })
+	let salt = crypto.randomBytes(16);
+	return await argon2.hash(password, {
+		...hashingConfig,
+		salt,
+	});
 }
- 
+
 async function verifyPasswordWithHash(password, hash) {
-    return await argon2.verify(hash, password, hashingConfig);
+	return await argon2.verify(hash, password, hashingConfig);
 }
 
 export function generateAccessToken(email, id) {
-    return jwt.sign({ userID: id, email: email }, process.env.TOKEN_SECRET, { expiresIn: '7200s' });
+	return jwt.sign({ userID: id, email: email }, process.env.TOKEN_SECRET, { expiresIn: '7200s' });
 }
 
 export async function authenticateAdminToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-  
-    if (token == null) return res.sendStatus(401);
-  
-    jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
-      console.log(err);
-  
-      if (err) return res.sendStatus(403);
-      if (!await isExistRegistration(user.email)) return res.sendStatus(401);
-      if (!await isAdmin(user.userID)) return res.sendStatus(401);
+	const authHeader = req.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
 
-      req.user = user;
-  
-      next();
-    })
+	if (token == null) return res.sendStatus(401);
+
+	jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
+
+		if (err) return res.sendStatus(403);
+		if (!(await isExistRegistration(user.email))) return res.sendStatus(401);
+		if (!(await isAdmin(user.userID))) return res.sendStatus(401);
+
+		req.user = user;
+
+		next();
+	});
 }
 
 export async function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-  
-    if (token == null) return res.sendStatus(401);
-  
-    jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
-      console.log(err);
-  
-      if (err) return res.sendStatus(403);
-      if (!await isExistRegistration(user.email)) return res.sendStatus(401);
-  
-      req.user = user;
-  
-      next();
-    })
+	const authHeader = req.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
+
+	if (token == null) return res.sendStatus(401);
+
+	jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
+
+		if (err) return res.sendStatus(403);
+		if (!(await isExistRegistration(user.email))) return res.sendStatus(401);
+
+		req.user = user;
+
+		next();
+	});
 }
 
 export async function isEmailValid(email) {
-    return emailValidator.validate({
-    email: email,
-    validateRegex: true,
-    validateMx: true,
-    validateTypo: true,
-    validateDisposable: true,
-    validateSMTP: false,    // Several email providers refuse SMTP
-  })
+	return emailValidator.validate({
+		email: email,
+		validateRegex: true,
+		validateMx: true,
+		validateTypo: true,
+		validateDisposable: true,
+		validateSMTP: false, // Several email providers refuse SMTP
+	});
 }
-  
+
 export async function isExistRegistration(email) {
-   const result = await getUserByEmail(email);
-    console.log(result);
-    if(result) {
-        console.log("van ilyen email");
-        return true;
-    }
-    console.log("nincs ilyen email");
-    return false;
+	const result = await getUserByEmail(email);
+	if (result) return true;
+	return false;
 }
-  
+
 export async function addUser(email, password) {
-    let result;
-    try {
-      const hash = await hashPassword(password);
-      const newuser = {
-        email: email,
-        password: hash,
-        userID: crypto.randomUUID(), 
-      }
-       result = await users.insertOne(newuser);
-      console.log(`A user was inserted with the _id: ${result.insertedId}`);
-      return result;
-    } finally {
-      //console.log("Database could not open");
-      //await client.close();
-    }
+	let result;
+	try {
+		const hash = await hashPassword(password);
+		const newuser = {
+			email: email,
+			password: hash,
+			userID: crypto.randomUUID(),
+		};
+		result = await users.insertOne(newuser);
+		return { code: 201, data: { message: "Created a new user.", userID: newuser.userID } };
+	} catch (error) {
+		return { code: 500, data: { message: 'Internal server error.' } };
+	}
 }
 
 export async function getUserByEmail(email) {
-    const result = await users.findOne( { email: { $eq: email } });
-    return result;
+	const result = await users.findOne({ email: { $eq: email } });
+	return result;
 }
 
 export async function isValidUser(email, password) {
-    const user = await getUserByEmail(email);
-    console.log(user);
-    if(!user) return { code: 401, data: { message: 'Bad credential.'}};
-    if(await verifyPasswordWithHash(password, user.password)) {
-        if(user.admin) {
-            return  { code: 200, data: { token: generateAccessToken(user.email, user.userID), admin: true }};
-        }
-        return  { code: 200, data: { token: generateAccessToken(user.email, user.userID) }};
-    }
-    return { code: 401, data: { message: 'Bad credential.'}};
+	const user = await getUserByEmail(email);
+	if (!user) return { code: 401, data: { message: 'Bad credential.' } };
+	if (await verifyPasswordWithHash(password, user.password)) {
+		if (user.admin) {
+			return { code: 200, data: { token: generateAccessToken(user.email, user.userID), admin: true } };
+		}
+		return { code: 200, data: { token: generateAccessToken(user.email, user.userID) } };
+	}
+	return { code: 401, data: { message: 'Bad credential.' } };
 }
 
 export async function findUserByUserID(userID) {
-    const result = await users.findOne({ userID: { $eq: userID }});
-    return result;
+	const result = await users.findOne({ userID: { $eq: userID } });
+	return result;
 }
 
 export async function setUserData(body, userInfo) {
-    let user;
-    let token = null;
-    let needToken; 
-    console.log("setuserdata");
-    console.log(body);
-    if(body.userID){ 
-        user = await findUserByUserID(body.userID);
-        needToken = false;
-    } else {
-        user = userInfo;
-        needToken = true;
-    }
-    console.log(user);
-    if(!user) return { code: 401, message: 'Bad credentials.'};
-    
-    const filter = { userID: user.userID };
-    if(body.newEmail && body.newPassword) {
-        if(await passwordStrength(body.newPassword).value !== 'Strong') {
-            return { code: 400, data: { message: 'Please provide a strong password.' }};
-          }
-          const { valid } = await isEmailValid(body.newEmail);
+	let user;
+	let token = null;
+	let needToken;
+	if (body.userID) {
+		try {
+			user = await findUserByUserID(body.userID);
+			needToken = false;
+		} catch (error) {
+			return { code: 500, data: { message: 'Internal server error.' } };
+		}
+	} else {
+		user = userInfo;
+		needToken = true;
+	}
+	if (!user) return { code: 401, message: 'Bad credentials.' };
+	const filter = { userID: user.userID };
+	if (body.newEmail && body.newPassword) {
+		if ((await passwordStrength(body.newPassword).value) !== 'Strong') {
+			return { code: 400, data: { message: 'Please provide a strong password.' } };
+		}
+		const { valid } = await isEmailValid(body.newEmail);
 
-          if (!valid) return { code: 400, data: { message: 'Please provide a valid email address.' }};
-    
-          if(await isExistRegistration(body.newEmail)) return { code: 400, data: { message: 'Email address already exist.'}};
-          try{
-            const hash = await hashPassword(body.newPassword);
-            const updateDocument = { $set: { email: body.newEmail, password: hash } };
-            await users.updateOne(filter, updateDocument);
-            if(needToken) token = generateAccessToken(body.newEmail, userInfo.userID);
-          } catch(error){
-            return { code: 500, data: { message: 'Internal server error.' }};
-        }  
-    } else if(body.newPassword) {
-        if(await passwordStrength(body.newPassword).value !== 'Strong') {
-          return { code: 400, data: { message: 'Please provide a strong password.' }};
-        }
-        try {
-            const hash = await hashPassword(body.newPassword);
-            const updateDocument = { $set: { password: hash } };
-            await users.updateOne(filter, updateDocument);
-        } catch(error){
-            return { code: 500, data: { message: error.message }};
-        }
-    } else if(body.newEmail) {
-      const { valid } = await isEmailValid(body.newEmail);
+		if (!valid) return { code: 400, data: { message: 'Please provide a valid email address.' } };
 
-      if (!valid) return { code: 400, data: { message: 'Please provide a valid email address.' }};
+		if (await isExistRegistration(body.newEmail)) return { code: 400, data: { message: 'Email address already exist.' } };
+		try {
+			const hash = await hashPassword(body.newPassword);
+			const updateDocument = { $set: { email: body.newEmail, password: hash } };
+			await users.updateOne(filter, updateDocument);
+			if (needToken) token = generateAccessToken(body.newEmail, userInfo.userID);
+		} catch (error) {
+			return { code: 500, data: { message: 'Internal server error.' } };
+		}
+	} else if (body.newPassword) {
+		if ((await passwordStrength(body.newPassword).value) !== 'Strong') {
+			return { code: 400, data: { message: 'Please provide a strong password.' } };
+		}
+		try {
+			const hash = await hashPassword(body.newPassword);
+			const updateDocument = { $set: { password: hash } };
+			await users.updateOne(filter, updateDocument);
+		} catch (error) {
+			return { code: 500, data: { message: 'Internal server error.' } };
+		}
+	} else if (body.newEmail) {
+		const { valid } = await isEmailValid(body.newEmail);
 
-      if(await isExistRegistration(body.newEmail)) return { code: 400, data: { message: 'Email address is already exist.'}};
-      try{
-        const updateDocument = { $set: { email: body.newEmail } };
-        await users.updateOne(filter, updateDocument);
-        if(needToken) token = generateAccessToken(body.newEmail, userInfo.userID);
-      } catch(error){
-        return { code: 500, data: { message: 'Internal server error.' }};
-        }  
-    }
-    if(token != null) return { code: 200, data: { message: 'Modify was successful.', token: token}};
-  return { code: 200, data: { message: 'Modify was successful.' }};
+		if (!valid) return { code: 400, data: { message: 'Please provide a valid email address.' } };
+
+		if (await isExistRegistration(body.newEmail)) return { code: 400, data: { message: 'Email address is already exist.' } };
+		try {
+			const updateDocument = { $set: { email: body.newEmail } };
+			await users.updateOne(filter, updateDocument);
+			if (needToken) token = generateAccessToken(body.newEmail, userInfo.userID);
+		} catch (error) {
+			return { code: 500, data: { message: 'Internal server error.' } };
+		}
+	}
+	if (token != null) return { code: 200, data: { message: 'Modify was successful.', token: token } };
+	return { code: 200, data: { message: 'Modify was successful.' } };
 }
 
 export async function isAdmin(userID) {
-    const user = await findUserByUserID(userID);
-    return (user.admin) ? true : false;
+	const user = await findUserByUserID(userID);
+	return user.admin ? true : false;
 }
 
 export async function getUsersForAdmin() {
-    try {
-        const listOfUsers = await users.find({}).toArray();
-        return { code: 200, data: listOfUsers };
-      } catch(error) {
-        return {code: 500, data: { message: 'Internal server error.'}};
-    }
+	try {
+		const listOfUsers = await users.find({}).toArray();
+		return { code: 200, data: listOfUsers };
+	} catch (error) {
+		return { code: 500, data: { message: 'Internal server error.' } };
+	}
 }
 
 export async function modifyUserByAdmin(body, userInfo) {
-    console.log(body);
-    try {
-        return await setUserData(body, userInfo);
-    } catch (error) {
-        return { code: 500, data: { message: "Internal server error" }};
-    }
+	try {
+		return await setUserData(body, userInfo);
+	} catch (error) {
+		return { code: 500, data: { message: 'Internal server error' } };
+	}
 }
 
 export async function deleteUserByAdmin(body) {
-    try {
-        const filter = { userID: body.userID };
-        await users.deleteOne(filter);
-        return { code: 204, data: { message: "Delete was successful." }};
-        } catch (error) {
-        return { code: 500, data: { message: "Internal server error" }};
-    }
+	try {
+		const filter = { userID: body.userID };
+		await users.deleteOne(filter);
+		return { code: 204, data: { message: 'Delete was successful.' } };
+	} catch (error) {
+		return { code: 500, data: { message: 'Internal server error' } };
+	}
 }
